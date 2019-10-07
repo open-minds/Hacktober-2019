e87c90e361125021c5a04f4e67b5ad60c81a21fa
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-10-07 19:59:19 +0100

    Prolog and play methode

--- Hello-World/python/MysteryNumber.py
+++ Hello-World/python/MysteryNumber.py
@@ -1,7 +1,29 @@
+import random
 
-
-
-
+def play(username="Alian"):
+    exit = False
+    score = 0
+    mysterNumber = random.randint(1, 100)
+    while(not(exit)):
+      userInput = input("Enter a number ")
+      print("User "+username+" say : "+userInput+"\n")
+      if(int(userInput) > mysterNumber):
+        print("Bot  : Go down \n")
+      elif int(userInput) > mysterNumber:
+          print("Bot : Go up \n")
+      else:
+          print("Bot : Congratulation\n")
+          score +=1
+          resp  = input("continue Yes or No ?\n")
+          if resp == "N":
+             exit = True
+          else:
+              mysterNumber = random.randint(1, 100)  
+    print(username+" Score "+score)
+    
+        
 if  __name__ == "__main__":
-    print("Welcome to our game for Hacktoberfest \n:")
-    play()
+    print("Welcome Hacktoberfest Mystery Number (:|?|:) \n:")
+    username = input("Enter your username")
+    print("Figure The mystery number from 1 to 100 GOOD LACK \n")
+    play(username)
