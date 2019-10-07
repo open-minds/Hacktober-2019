e8fb5f6ef57803ac76ca169c460fad9bd9b9687f
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-10-07 20:02:28 +0100

    Add display username and fixing second condition

--- Hello-World/python/MysteryNumber.py
+++ Hello-World/python/MysteryNumber.py
@@ -4,26 +4,27 @@ def play(username="Alian"):
     exit = False
     score = 0
     mysterNumber = random.randint(1, 100)
+    print
     while(not(exit)):
       userInput = input("Enter a number ")
       print("User "+username+" say : "+userInput+"\n")
       if(int(userInput) > mysterNumber):
         print("Bot  : Go down \n")
-      elif int(userInput) > mysterNumber:
+      elif int(userInput) < mysterNumber:
           print("Bot : Go up \n")
       else:
           print("Bot : Congratulation\n")
           score +=1
           resp  = input("continue Yes or No ?\n")
-          if resp == "N":
+          if resp == "No":
              exit = True
           else:
               mysterNumber = random.randint(1, 100)  
     print(username+" Score "+score)
-    
-        
+
+
 if  __name__ == "__main__":
-    print("Welcome Hacktoberfest Mystery Number (:|?|:) \n:")
-    username = input("Enter your username")
-    print("Figure The mystery number from 1 to 100 GOOD LACK \n")
+    print("Welcome Hacktoberfest Mystery Number (:|?|:) \n")
+    username = input("Enter your username ")
+    print("Figure The mystery number from 1 to 100 GOOD LACK "+username+" \n")
     play(username)
