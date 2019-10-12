b291b954241a15a2943d64eae352c4ada6c46d8a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-10-12 14:04:00 +0100

    adding APIs file & TodoApi

---
+++ APIs/TodoAPI/TodoAPI/Migrations/20190611004507_addingColorAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoAPI.Migrations
+{
+    public partial class addingColorAttribute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "toDoItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "toDoItems");
+        }
+    }
+}
---
+++ APIs/TodoAPI/TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public int id { get; set; }
+        public bool status { get; set; }
+        public string task { get; set; }
+        public string color { get; set; }
+    }
+}
